749efab47b7276c82c51aa134dcd9688bbe5c044
Timo Osterkamp <tosterkamp@uos.de>
2020-09-02 16:08:12 +0200

    set nitter rss link to ignore retweets

--- rssbots/rssbots.py
+++ rssbots/rssbots.py
@@ -22,5 +22,5 @@ print("IGMetall")
 rss2toot.toot('https://www.igmetall-osnabrueck.de/rss.xml', 'https://osna.social', appname='igmetall')
 
 print("NOZ")
-rss2toot.toot('https://nitter.net/noz_os/rss', 'https://osna.social', appname='noz')
+rss2toot.toot('https://nitter.net/noz_os/rss?f=tweets&e-replies=on&e-nativeretweets=on', 'https://osna.social', appname='noz')
 
